70a14553176f81e38a0344c9bc2f9f78ee0de2e5
poolbuyr <poolbuyr@users.noreply.github.com>
2026-08-13 17:36:51 +0000

    deploy: poolbuyr/poolbuyr@64f1a2267b01a290af7164f386147161206fb4b8

--- branches/explainer-animations/assets/chevron-down-jVL215bu.js
+++
@@ -1,6 +0,0 @@
-import{F as o}from"./index-qnoB9Sc4.js";/**
- * @license lucide-react v1.27.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const n=[["path",{d:"m6 9 6 6 6-6",key:"qrunsl"}]],c=o("chevron-down",n);export{c as C};
